c3f98162417a346c245f8d814247f272c55c0783
Eb Heravi <ebheravi159@gmail.com>
2020-12-08 15:59:28 +0330

    Create put request

--- app.js
+++ app.js
@@ -61,6 +61,13 @@ app.delete('/items/:id', (req, res) => {
   })
 })
 
+app.put('/items/:id', (req, res) => {
+  const id = req.params.id;
+  Item.findByIdAndUpdate(id, req.body).then(result => {
+    res.json({ msg: 'Updated Successfully' })
+  })
+})
+
 app.use((req, res) => {
   res.render('error-page')
 })

49baa3980af70a7b4499f531817cede6458fb886
Eb Heravi <ebheravi159@gmail.com>
2020-12-08 12:35:03 +0330

    Getting and savng data

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const Item = require('./models/items');
 const app = express();
 const mongodb = 'mongodb+srv://sa:Password123@cluster0.iscuv.mongodb.net/item-database?retryWrites=true&w=majority';
 mongoose.connect(mongodb, {
@@ -9,8 +10,27 @@ mongoose.connect(mongodb, {
   console.log('connected')
   app.listen(5000);
 }).catch(err => console.log(err))
+
 app.set('view engine', 'ejs');
 
+
+app.get('/create-item', (req, res) => {
+  const item = new Item({
+    name: 'computer',
+    price: 2000
+  });
+  item.save().then(result => res.send(result)).catch(err => console.log(err))
+})
+
+app.get('/get-items', (req, res) => {
+  Item.find().then(result => res.send(result)).catch(err => console.log(err))
+
+})
+
+app.get('/get-item', (req, res) => {
+  Item.findById("5fcf3da401f19b298c0b7106").then(result => res.send(result)).catch(err => console.log(err))
+
+})
 
 
 app.get('/', (req, res) => {
